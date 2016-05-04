ed8ebf7882b9eaa620d95e4ded81b1c4d8b3df5f
Vítor Marques de Faria <vtrmrx@gmail.com>
2016-05-04 09:20:55 -0300

    bug fix: reader responsivo em todas as páginas

---
+++ scripts/header-responsive.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$('#nav-icon').click(function(){
+		$(this).toggleClass('open');
+		$('#menu-mobile').toggleClass('open');
+		$('body').toggleClass('fixed');
+		$('header').toggleClass('blurred');
+		$('main').toggleClass('blurred');
+		$('footer').toggleClass('blurred');
+	});
+});
+
+var mobmenu = document.querySelector("#nav-icon");
+var reset = function(reset){
+	if (mobmenu.className == 'open') {
+		$('#nav-icon').toggleClass('open');
+		$('#menu-mobile').toggleClass('open');
+		$('body').toggleClass('fixed');
+		$('header').toggleClass('blurred');
+		$('main').toggleClass('blurred');
+		$('footer').toggleClass('blurred');
+	}
+}
+
+window.addEventListener('resize', reset);
--- scripts/header-shrink.js
+++ scripts/header-shrink.js
@@ -15,28 +15,3 @@
 
 // window.addEventListener('scroll', shrink);
 // window.addEventListener('resize', shrink);
-
-$(document).ready(function(){
-	$('#nav-icon').click(function(){
-		$(this).toggleClass('open');
-		$('#menu-mobile').toggleClass('open');
-		$('body').toggleClass('fixed');
-		$('header').toggleClass('blurred');
-		$('main').toggleClass('blurred');
-		$('footer').toggleClass('blurred');
-	});
-});
-
-var mobmenu = document.querySelector("#nav-icon");
-var reset = function(reset){
-	if (mobmenu.className == 'open') {
-		$('#nav-icon').toggleClass('open');
-		$('#menu-mobile').toggleClass('open');
-		$('body').toggleClass('fixed');
-		$('header').toggleClass('blurred');
-		$('main').toggleClass('blurred');
-		$('footer').toggleClass('blurred');
-	}
-}
-
-window.addEventListener('resize', reset);
