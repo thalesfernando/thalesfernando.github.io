be60674c8bfa09808b64004ceb275a915fc6b0f1
Vítor Marques de Faria <vtrmrx@gmail.com>
2016-07-13 08:26:55 -0300

    home nova

--- scripts/header-responsive.js
+++ scripts/header-responsive.js
@@ -2,7 +2,7 @@ var breakpoint = 24;
 var distanceY = window.pageYOffset || document.documentElement.scrollTop;
 var mobmenu = document.querySelector("#nav-icon");
 var extraHeight = $('body').height() - $(window).height();
-console.log(extraHeight);
+// console.log(extraHeight);
 
 var scrolling;
 var st;
@@ -14,7 +14,7 @@ var hiddenHeader = false;
 var noTransition = true;
 var headerHeight = $('header').height();
 var refPoint = window.pageYOffset;
-console.log("move from " + refPoint);
+// console.log("move from " + refPoint);
 
 $(document).ready(function(){
 	$('#nav-icon').click(function(){
@@ -39,7 +39,7 @@ $(document).ready(function(){
 var reset = function(reset) {
 	headerHeight = $('header').height();
 	extraHeight = $('body').height() - $(window).height();
-	console.log(extraHeight);
+	// console.log(extraHeight);
 	if (mobmenu.className == 'xis') {
 		$('#nav-icon').toggleClass('xis');
 		$('#menu-mobile-bg').toggleClass('open');
@@ -56,9 +56,9 @@ $(window).scroll(function() {
 	    clearTimeout($.data(this, 'scrollTimer'));
 	    $.data(this, 'scrollTimer', setTimeout(function() {
 	        scrolling = false;
-	        console.log("");
-	        console.log("now stopped");
-	        console.log("");
+	        // console.log("");
+	        // console.log("now stopped");
+	        // console.log("");
 	        if (hiddenHeader == false) {
 	        	$('header').css("top", "0px");
 	        }
@@ -66,7 +66,7 @@ $(window).scroll(function() {
 
 	    if (scrolling == false ) { //it means: if scrolling was false right before current scroll event check
 		   	refPoint = window.pageYOffset;
-		   	console.log("move from " + refPoint);
+		   	// console.log("move from " + refPoint);
 		}
 
 		if (noTransition == false) {
@@ -82,15 +82,15 @@ $(window).scroll(function() {
 
 	   	if (st > lastScrollTop){
 	    	if ((st - refPoint) > headerHeight) {
-	    		console.log("header is hidden!");
+	    		// console.log("header is hidden!");
 	    		$('header').css("top", -headerHeight);
 	    		hiddenHeader = true;
 	    		noTransition = false;
 	        } else {
 	        	if (hiddenHeader == false) {
 	        		noTransition = true;
-			    	console.log("scrolling down: gradually hiding header...");
-			    	console.log(refPoint);
+			    	// console.log("scrolling down: gradually hiding header...");
+			    	// console.log(refPoint);
 			    	$('header').css("top",(-(st - refPoint))+"px");
 			    }
 	        }
@@ -99,17 +99,17 @@ $(window).scroll(function() {
 	   	}
 	   	if (st < lastScrollTop){
 	   		upscroll = true;
-	   		console.log("scrolling up")
+	   		// console.log("scrolling up");
 	   	}
 
 
 	   	deslocamento = st - lastScrollTop;
 	   	lastScrollTop = st;
 	   	if ((lastD * deslocamento) < 0){
-	    	console.log("(changed direction)");
+	    	// console.log("(changed direction)");
 	    	if (upscroll == true){
-	    		console.log("show header!");
-	    		console.log(refPoint);
+	    		// console.log("show header!");
+	    		// console.log(refPoint);
 	    		$('header').css("top", "0px");
 	    		hiddenHeader = false;
 	    	}
